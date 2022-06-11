ece7d23b22b75ae8d782fa4c7c2e6f58acf96281
Park <novo200297@gmail.com>
2022-06-11 19:02:58 +0900

    지도에 휴게소 위치 표시

---
+++ 박종빈/test/app/src/main/java/com/example/test/locationInfo.java
@@ -0,0 +1,31 @@
+package com.example.test;
+
+public class locationInfo {
+    private String location;
+    private String x;
+    private String y;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getX() {
+        return x;
+    }
+
+    public void setX(String x) {
+        this.x = x;
+    }
+
+    public String getY() {
+        return y;
+    }
+
+    public void setY(String y) {
+        this.y = y;
+    }
+}
