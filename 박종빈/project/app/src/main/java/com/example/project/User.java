5e6dd7887e0e6931255793b4b4f4ad96af8aea4c
RuiFoot <95283019+RuiFoot@users.noreply.github.com>
2022-06-19 14:28:34 +0900

    Update User.java
    주석 추가

--- 박종빈/project/app/src/main/java/com/example/project/User.java
+++ 박종빈/project/app/src/main/java/com/example/project/User.java
@@ -1,9 +1,10 @@
 package com.example.project;
 
+//listview를 위한 목록 리스트 생성
 public class User {
 
-    String restName, location, lastMsgTime;
-    int imageId;
+    String restName, location, lastMsgTime; // 휴게소 명, 위치, 일련번호 
+    int imageId; //이미지 주소
 
     public User(String restName, String location, String lastMsgTime, int imageId) {
         this.restName = restName;
